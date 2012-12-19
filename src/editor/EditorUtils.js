f8820f7bb47c1700a1b4d8c696e93aafccffc55f
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-12-19 18:37:40 +0100

    Added the Languages module to have a centralized place for adding new languages The LESS extension now uses this exclusively (after manually loading the CodeMirror mode)

--- src/editor/EditorUtils.js
+++ src/editor/EditorUtils.js
@@ -51,6 +51,8 @@ define(function (require, exports, module) {
     require("thirdparty/CodeMirror2/mode/yaml/yaml");
     require("thirdparty/CodeMirror2/mode/haxe/haxe");
 
+    var Languages = require("language/Languages");
+
     /**
      * @private
      * Given a file URL, determines the mode to use based
@@ -69,6 +71,11 @@ define(function (require, exports, module) {
         
         // Make checks below case-INsensitive
         ext = ext.toLowerCase();
+
+        var language = Languages.getLanguageForFileExtension(ext);
+        if (language) {
+            return language.mode.CodeMirror;
+        }
 
         switch (ext) {
 
